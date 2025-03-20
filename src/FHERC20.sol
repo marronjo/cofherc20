ee7fad2b3d94192e9910fdccff6e7fa9c5808a58
Joe Marron <marronjo@tcd.ie>
2025-03-20 21:42:51 +0900

    add simple ERC20 contract

---
+++ src/FHERC20.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract FHERC20 is ERC20 {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+}
