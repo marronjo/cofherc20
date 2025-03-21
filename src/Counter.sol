c8bf9b2cb4f34785b33b24880b59fadb5bbb9b8d
Joe Marron <marronjo@tcd.ie>
2025-03-21 21:10:27 +0900

    fix dependency versions

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,14 +1,53 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Counter {
-    uint256 public number;
+import "@fhenixprotocol/cofhe-contracts/FHE.sol";
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+contract Counter {
+    euint32 public eNumber;
+
+    function setNumber(InEuint32 memory inNumber) public {
+        eNumber = FHE.asEuint32(inNumber);
+        FHE.allowThis(eNumber);
     }
 
     function increment() public {
-        number++;
+        eNumber = FHE.add(eNumber, FHE.asEuint32(1));
+        FHE.allowThis(eNumber);
+    }
+
+    function add(InEuint32 memory inNumber) public {
+        eNumber = FHE.add(eNumber, FHE.asEuint32(inNumber));
+        FHE.allowThis(eNumber);
+    }
+
+    function sub(InEuint32 memory inNumber) public {
+        euint32 inAsEuint32 = FHE.asEuint32(inNumber);
+        euint32 eSubOrZero = FHE.select(
+            FHE.lte(inAsEuint32, eNumber),
+            inAsEuint32,
+            FHE.asEuint32(0)
+        );
+        eNumber = FHE.sub(eNumber, eSubOrZero);
+        FHE.allowThis(eNumber);
+    }
+
+    function mul(InEuint32 memory inNumber) public {
+        eNumber = FHE.mul(eNumber, FHE.asEuint32(inNumber));
+        FHE.allowThis(eNumber);
+    }
+
+    function decrypt() public {
+        FHE.decrypt(eNumber);
+    }
+
+    function getDecryptResult(euint32 input1) public view returns (uint32) {
+        return FHE.getDecryptResult(input1);
+    }
+
+    function getDecryptResultSafe(
+        euint32 input1
+    ) public view returns (uint32 value, bool decrypted) {
+        return FHE.getDecryptResultSafe(input1);
     }
 }
--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -1,24 +1,94 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Test, console} from "forge-std/Test.sol";
+import {Test} from "forge-std/Test.sol";
 import {Counter} from "../src/Counter.sol";
+import {CoFheTest} from "@fhenixprotocol/cofhe-foundry-mocks/CoFheTest.sol";
+import "@fhenixprotocol/cofhe-contracts/FHE.sol";
 
 contract CounterTest is Test {
+    CoFheTest CFT;
+    address bob = makeAddr("bob");
+
     Counter public counter;
 
     function setUp() public {
+        CFT = new CoFheTest(false);
+
         counter = new Counter();
-        counter.setNumber(0);
+
+        // Set number to 5
+        InEuint32 memory inNumber = CFT.createInEuint32(5, bob);
+        vm.prank(bob);
+        counter.setNumber(inNumber);
     }
 
-    function test_Increment() public {
+    function test_setNumber() public {
+        InEuint32 memory inNumber = CFT.createInEuint32(10, bob);
+        vm.prank(bob);
+        counter.setNumber(inNumber);
+        CFT.assertHashValue(counter.eNumber(), 10);
+    }
+
+    function test_increment() public {
         counter.increment();
-        assertEq(counter.number(), 1);
+        CFT.assertHashValue(counter.eNumber(), 6);
     }
 
-    function testFuzz_SetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+    function test_add() public {
+        InEuint32 memory inNumber = CFT.createInEuint32(2, bob);
+        vm.prank(bob);
+        counter.add(inNumber);
+        CFT.assertHashValue(counter.eNumber(), 7);
+    }
+
+    function test_sub() public {
+        InEuint32 memory inNumber = CFT.createInEuint32(3, bob);
+        vm.prank(bob);
+        counter.sub(inNumber);
+        CFT.assertHashValue(counter.eNumber(), 2);
+    }
+
+    function test_mul() public {
+        InEuint32 memory inNumber = CFT.createInEuint32(2, bob);
+        vm.prank(bob);
+        counter.mul(inNumber);
+        CFT.assertHashValue(counter.eNumber(), 10);
+    }
+
+    function test_decrypt() public {
+        CFT.assertHashValue(counter.eNumber(), 5);
+        counter.decrypt();
+
+        uint8 count = 0;
+        bool success = false;
+        while (!success && count < 100) {
+            try counter.getDecryptResult(counter.eNumber()) returns (uint32) {
+                success = true;
+            } catch {
+                vm.warp(block.timestamp + 1);
+                count += 1;
+            }
+        }
+    }
+
+    function test_decryptSafe() public {
+        CFT.assertHashValue(counter.eNumber(), 5);
+        counter.decrypt();
+
+        uint8 count = 0;
+        bool success = false;
+        while (!success && count < 100) {
+            (uint256 result, bool decrypted) = counter.getDecryptResultSafe(
+                counter.eNumber()
+            );
+            if (decrypted) {
+                assertEq(result, 5);
+                success = true;
+            } else {
+                vm.warp(block.timestamp + 1);
+                count += 1;
+            }
+        }
     }
 }
